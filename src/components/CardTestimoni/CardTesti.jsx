72513d0e22b4aade72f01768eb705e3b8aade382
fauziahns <fauziahnsf@gmail.com>
2023-05-31 11:55:23 +0700

    fixing: cardTesti

--- src/components/CardTestimoni/CardTesti.jsx
+++ src/components/CardTestimoni/CardTesti.jsx
@@ -4,7 +4,7 @@ import assets from '../../assets/assets'
 export default function CardTesti(props) {
     const { nama, tanggal, testimoni } = props
   return (
-    <div className='bg-[#F9FAFB] border border-[#D2D7E0] rounded-lg max-w-[684px]'>
+    <div className='bg-[#F9FAFB] border border-[#D2D7E0] rounded-xl max-w-[684px]'>
       <div className="flex gap-3 p-[16px]">
         <div className="">
             <img src={assets.profile} alt="" />
